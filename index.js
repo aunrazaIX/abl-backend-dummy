8f802cf9b49bd509cd9d3e69acaba28feb902bf7
Aun Raza <aun.raza@integrationxperts.com>
2025-01-21 14:15:25 +0500

    adada

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.use(cors());
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.json());
+app.use('/api', require('./api'));
 
 var server = http.createServer(app);
 
